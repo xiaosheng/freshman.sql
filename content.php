683734c462ab95ab331bd81b8c05056243cd2c0f
xiaosheng <1136533569@qq.com>
2013-07-13 22:20:50 +0800

    the code to import data

---
+++ content.php
@@ -0,0 +1,60 @@
+<?php
+	$con = mysql_connect('localhost','root','');
+	if(!$con){
+		die('could not connect:'.mysql_errno());
+	}
+	
+	mysql_select_db('freshman',$con);
+	mysql_query("SET NAMES 'utf8'");
+	$name = "SELECT * FROM dc_newsinfo WHERE catid = 37";
+	$result = mysql_query($name);
+	while($row = mysql_fetch_object($result)){
+			$sam = mysql_query("INSERT INTO fm_posts (title,content,source,author_id,created_date,status)
+			VALUES('$row->title','$row->newscontent','$row->source','1','$row->addtime','1')");
+		$name1 = "SELECT `id` FROM fm_posts where title= '$row->title'";
+		$name2 = mysql_query($name1,$con);
+		$name3 = mysql_fetch_array($name2);
+		mysql_query("INSERT INTO fm_posts_categories(post_id,category_id) VALUES ($name3[0],'1')");
+		 mysql_query("INSERT INTO fm_posts_tags (post_id,tag_id) VALUES ($name3[0],'1')");
+
+		$naem = mysql_query("INSERT INTO `fm_post_metas`(`post_id`,`key`,`value`) VALUES ($name3[0],'campus','龙洞')");
+	}
+
+	$name = "SELECT * FROM dc_newsinfo WHERE catid = 38";
+	$result = mysql_query($name);
+	while($row = mysql_fetch_object($result)){
+			$sam = mysql_query("INSERT INTO fm_posts (title,content,source,author_id,created_date,status)
+			VALUES('$row->title','$row->newscontent','$row->source','1','$row->addtime','1')");
+		$name1 = "SELECT `id` FROM fm_posts where title= '$row->title'";
+		$name2 = mysql_query($name1,$con);
+		$name3 = mysql_fetch_array($name2);
+		mysql_query("INSERT INTO fm_posts_categories(post_id,category_id) VALUES ($name3[0],'2')");
+		 mysql_query("INSERT INTO fm_posts_tags(post_id,tag_id) VALUES ($name3[0],'1')");
+		$naem = mysql_query("INSERT INTO `fm_post_metas`(`post_id`,`key`,`value`) VALUES ($name3[0],'campus','龙洞')");
+	}
+
+	$name = "SELECT * FROM dc_newsinfo WHERE catid = 40";
+	$result = mysql_query($name);
+	while($row = mysql_fetch_object($result)){
+			$sam = mysql_query("INSERT INTO fm_posts (title,content,source,author_id,created_date,status)
+			VALUES('$row->title','$row->newscontent','$row->source','1','$row->addtime','1')");
+		$name1 = "SELECT `id` FROM fm_posts where title= '$row->title'";
+		$name2 = mysql_query($name1,$con);
+		$name3 = mysql_fetch_array($name2);
+		mysql_query("INSERT INTO fm_posts_categories(post_id,category_id) VALUES ($name3[0],'3')");
+		 mysql_query("INSERT INTO fm_posts_tags(post_id,tag_id) VALUES ($name3[0],'1')");
+		$naem = mysql_query("INSERT INTO `fm_post_metas`(`post_id`,`key`,`value`) VALUES ($name3[0],'campus','龙洞')");	
+	}
+
+	$name = "SELECT * FROM dc_newsinfo WHERE catid = 41";
+	$result = mysql_query($name);
+	while($row = mysql_fetch_object($result)){
+			$sam = mysql_query("INSERT INTO fm_posts (title,content,source,author_id,created_date,status)
+			VALUES('$row->title','$row->newscontent','$row->source','1','$row->addtime','1')");
+		$name1 = "SELECT `id` FROM fm_posts where title= '$row->title'";
+		$name2 = mysql_query($name1,$con);
+		$name3 = mysql_fetch_array($name2);
+		mysql_query("INSERT INTO fm_posts_categories(post_id,category_id) VALUES ($name3[0],'4')");
+		 mysql_query("INSERT INTO fm_posts_tags(post_id,tag_id) VALUES ($name3[0],'1')");
+		$naem = mysql_query("INSERT INTO `fm_post_metas`(`post_id`,`key`,`value`) VALUES ($name3[0],'campus','龙洞')");
+	}
